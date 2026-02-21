fc4f443e8881bb4bd050773b8c4b5e9eb30f71c3
Kenny-Stone <kennystone2006@gmail.com>
2026-02-21 12:18:27 +0000

    Additional design for student dashboard

---
+++ src/com/studentinformationmanagementsystem/StudentDashboard.java
@@ -0,0 +1,12 @@
+package com.studentinformationmanagementsystem;
+
+import javax.swing.*;
+
+public class StudentDashboard {
+    private JPanel studentDashboardPanel;
+    private JPanel header;
+    private JLabel logo;
+    private JLabel studentDetailsLink;
+    private JPanel stuImageAndWelcomePanel;
+    private JLabel studentImage;
+}
